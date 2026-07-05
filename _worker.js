b414b31c941d00c5a50cb5335edc7ebee7512a1c
1988xlp <86917078+1988xlp@users.noreply.github.com>
2026-07-05 14:37:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ let addressesnotls = [];
 let addressesnotlsapi = [];
 
 let addressescsv = [];
-let DLS = 1;
+let DLS = 4;
 let remarkIndex = 1;//CSV备注所在列偏移量
 
 let subConverter = 'SUBAPI.cmliussss.net';
