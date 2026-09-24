6c77cb9651318f0ea78a8fe9414a914a99f0b586
1988xlp <86917078+1988xlp@users.noreply.github.com>
2026-09-24 23:09:34 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 
 let 快速订阅访问入口 = ['auto'];
 let addresses = [];
-let addressesapi = ["https://cdn.jsdelivr.net/gh/1988xlp/WorkerVless2sub@main/ALL-2026-09-20.txt?v=20260920"];
+let addressesapi = ["https://cdn.jsdelivr.net/gh/1988xlp/WorkerVless2sub@main/ALL-2026-09-25.txt?v=20260925"];
 
 let addressesnotls = [];
 let addressesnotlsapi = [];
